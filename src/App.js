7abd8988f533d09bbf079005a61260104bc2abc2
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-09-02 14:59:53 -0400

    Add basic API call to test server connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://roop-verma-archive.herokuapp.com/api/raags')
+      .then((response) => {
+        const res = response.data;
+        setData(res);
+      });
+  }, []);
+
+  const dataMap = () => {
+    console.log('mapping data');
+    data.map((item, index) => {
+      console.log(item.name);
+      return <li key={index}>{item.name}</li>;
+    });
+  };
+
   return (
     <div>
       <h1>Welcome to the Roop Verma Digital Archive</h1>
+      <p>Some names of raags:</p>
+      <ul>
+        {data.map((item, index) => {
+          console.log(item.name);
+          return <li key={index}>{item.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
