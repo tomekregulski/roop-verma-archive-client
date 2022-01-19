8783bc75d977f9f721144add7f3c51546c97b9fb
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2022-01-19 18:02:52 -0500

    Add setting JWT in AuthContext

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -5,6 +5,26 @@ export const AuthContext = createContext();
 export const AuthProvider = (props) => {
   const [isAuth, setIsAuth] = useState(false);
   const [userData, setUserData] = useState({});
+  const [jwt, setJwt] = useState('');
+
+  useEffect(() => {
+    console.log('cookies');
+    const allCookies = document.cookie.split('; ');
+    let cookies = {};
+    for (let i = 0; i < allCookies.length; i++) {
+      const currentCookie = allCookies[i].split('=');
+      cookies[currentCookie[0]] = currentCookie[1];
+    }
+    console.log(allCookies);
+    console.log(cookies);
+    const jwtKey = 'roop-verma-library';
+    let currentJwt;
+    if (Object.keys(cookies).includes(jwtKey)) {
+      currentJwt = cookies[jwtKey];
+      setJwt(currentJwt);
+      console.log(currentJwt);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider
