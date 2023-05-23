4abf6c7ac4c161def412a93937dcc56ea097abae
Tomek Regulski <tomek.regulski@gmail.com>
2023-05-22 20:27:32 -0400

    add account management

--- src/views/ManageAccount.tsx
+++ src/views/ManageAccount.tsx
@@ -1,28 +1,36 @@
 import axios from 'axios';
 
+import { useAuthContext } from '../context/AuthContext';
+
 // import tw from 'twin.macro';
 
+const key = import.meta.env.VITE_API_KEY;
+
 export function ManageAccount() {
+  const { userData } = useAuthContext();
+
   const handlePortal = async () => {
     console.log('portal');
-    const portalRes = await axios.post(
-      `${import.meta.env.VITE_API_URL}/users/create-portal-session`,
-      {
-        customerId: '12345',
-      },
-    );
-    console.log(portalRes);
+    console.log(userData);
+    try {
+      const portalRes = await axios.post(
+        `${import.meta.env.VITE_API_ORIGIN}/api/v1/user/create-portal-session/${key}`,
+        {
+          customerId: userData!.stripeId,
+        },
+      );
+      const portalAddress = portalRes.data.session.url;
+      console.log(portalAddress);
+      window.location.href = portalAddress;
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
-    <>
-      <button
-        id="portal-session-button"
-        type="button"
-        // css={tw`h-[100px] w-[200px] bg-blue-200`}
-        onClick={() => handlePortal()}
-      >
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <button id="portal-session-button" type="button" onClick={() => handlePortal()}>
         Manage Account
       </button>
-    </>
+    </div>
   );
 }
