b6fb58035cb3c7920d39594502920fb866fd8a50
Tomek Regulski <tomek.regulski@gmail.com>
2023-07-15 00:54:54 -0400

    update styles. add vimeo support

--- src/views/testimonials/TextContent.tsx
+++ src/views/testimonials/TextContent.tsx
@@ -1,24 +1,26 @@
+import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 
 import { Testimonial } from './Testimonials';
+
+const TextTestimonialContainer = styled.div();
+const TextTestimonialContent = styled.div();
 
 export function TextContent(props: { testimonial: Testimonial }) {
   const { testimonial } = props;
   const { content, id } = testimonial;
 
-  console.log(id);
-
   return (
-    <div>
-      <div
+    <TextTestimonialContainer className="flex flex-col items-start justify-start h-full w-full p-[20px]">
+      <TextTestimonialContent
         id={`testimonial-card-${id}`}
-        className="text-center mt-8 line-clamp-6 overflow-hidden overflow-ellipsis"
+        className="text-left mt-8 line-clamp-6 overflow-hidden overflow-ellipsis"
       >
         {content}
-      </div>
+      </TextTestimonialContent>
       <Link to={`/testimonials/${id}`} state={{ testimonial }}>
         See More
       </Link>
-    </div>
+    </TextTestimonialContainer>
   );
 }
